eb1aaf53be015bc32bf85de4c0c3b97d04e0b018
Speykious <speykious@gmail.com>
2021-11-05 15:14:01 +0100

    Add very rudimentary example
    99% of it comes from https://github.com/Ruslan-B/FFmpeg.AutoGen/tree/master/FFmpeg.AutoGen.Example.

---
+++ SeeShark.Example/Program.cs
@@ -0,0 +1,145 @@
+﻿using System.Runtime.InteropServices;
+using FFmpeg.AutoGen;
+
+namespace SeeShark.Example
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Usage: dotnet run <camera-device>");
+                return;
+            }
+
+            var cameraDevice = args[0];
+
+            Console.WriteLine("Current directory: " + Environment.CurrentDirectory);
+            Console.WriteLine("Running in {0}-bit mode.", Environment.Is64BitProcess ? "64" : "32");
+
+            // FFmpegBinariesHelper.RegisterFFmpegBinaries();
+            ffmpeg.RootPath = "/usr/lib";
+
+            Console.WriteLine($"FFmpeg version info: {ffmpeg.av_version_info()}");
+
+            SetupLogging();
+            ConfigureHWDecoder(out var deviceType);
+
+            Console.WriteLine("Decoding...");
+            DecodeAllFramesToImages(deviceType, cameraDevice);
+        }
+
+        private static void ConfigureHWDecoder(out AVHWDeviceType HWtype)
+        {
+            HWtype = AVHWDeviceType.AV_HWDEVICE_TYPE_NONE;
+            Console.Write("Use hardware acceleration for decoding? [y/N] ");
+            var key = Console.ReadLine();
+            var availableHWDecoders = new Dictionary<int, AVHWDeviceType>();
+
+            if (key == "y")
+            {
+                Console.WriteLine("Select hardware decoder:");
+                var type = AVHWDeviceType.AV_HWDEVICE_TYPE_NONE;
+                var number = 0;
+
+                while ((type = ffmpeg.av_hwdevice_iterate_types(type)) != AVHWDeviceType.AV_HWDEVICE_TYPE_NONE)
+                {
+                    Console.WriteLine($"{++number}. {type}");
+                    availableHWDecoders.Add(number, type);
+                }
+
+                if (availableHWDecoders.Count == 0)
+                {
+                    Console.WriteLine("Your system have no hardware decoders.");
+                    HWtype = AVHWDeviceType.AV_HWDEVICE_TYPE_NONE;
+                    return;
+                }
+
+                var decoderNumber = availableHWDecoders
+                    .SingleOrDefault(t => t.Value == AVHWDeviceType.AV_HWDEVICE_TYPE_DXVA2).Key;
+                if (decoderNumber == 0)
+                    decoderNumber = availableHWDecoders.First().Key;
+                Console.Write($"Selected [{decoderNumber}] ");
+                int.TryParse(Console.ReadLine(), out var inputDecoderNumber);
+                availableHWDecoders.TryGetValue(inputDecoderNumber == 0 ? decoderNumber : inputDecoderNumber,
+                    out HWtype);
+            }
+        }
+
+        private static unsafe void SetupLogging()
+        {
+            ffmpeg.av_log_set_level(ffmpeg.AV_LOG_VERBOSE);
+
+            // do not convert to local function
+            av_log_set_callback_callback logCallback = (p0, level, format, vl) =>
+            {
+                if (level > ffmpeg.av_log_get_level()) return;
+
+                var lineSize = 1024;
+                var lineBuffer = stackalloc byte[lineSize];
+                var printPrefix = 1;
+                ffmpeg.av_log_format_line(p0, level, format, vl, lineBuffer, lineSize, &printPrefix);
+                var line = Marshal.PtrToStringAnsi((IntPtr) lineBuffer);
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(line);
+                Console.ResetColor();
+            };
+
+            ffmpeg.av_log_set_callback(logCallback);
+        }
+
+        private static unsafe void DecodeAllFramesToImages(AVHWDeviceType HWDevice, string url)
+        {
+            using var vsd = new CameraStreamDecoder("v4l2", url, HWDevice);
+
+            Console.WriteLine($"codec name: {vsd.CodecName}");
+
+            var info = vsd.GetContextInfo();
+            info.ToList().ForEach(x => Console.WriteLine($"{x.Key} = {x.Value}"));
+
+            var sourcePixelFormat = HWDevice == AVHWDeviceType.AV_HWDEVICE_TYPE_NONE
+                ? vsd.PixelFormat
+                : GetHWPixelFormat(HWDevice);
+            var destinationPixelFormat = AVPixelFormat.AV_PIX_FMT_RGB24;
+            using var vfc = new VideoFrameConverter(
+                vsd.FrameWidth, vsd.FrameHeight, sourcePixelFormat,
+                vsd.FrameWidth, vsd.FrameHeight, destinationPixelFormat
+            );
+
+            var frameNumber = 0;
+
+            while (vsd.TryDecodeNextFrame(out var frame))
+            {
+                var convertedFrame = vfc.Convert(frame);
+
+                var a = convertedFrame.data.ToArray().Cast<byte[]>().ToArray();
+                
+                // At this point I do nothing and put a breakpoint to see if it casts.
+                // ... It doesn't :'(
+
+                Console.WriteLine($"frame: {frameNumber}");
+                frameNumber++;
+            }
+        }
+
+        private static AVPixelFormat GetHWPixelFormat(AVHWDeviceType hWDevice)
+        {
+            return hWDevice switch
+            {
+                AVHWDeviceType.AV_HWDEVICE_TYPE_NONE => AVPixelFormat.AV_PIX_FMT_NONE,
+                AVHWDeviceType.AV_HWDEVICE_TYPE_VDPAU => AVPixelFormat.AV_PIX_FMT_VDPAU,
+                AVHWDeviceType.AV_HWDEVICE_TYPE_CUDA => AVPixelFormat.AV_PIX_FMT_CUDA,
+                AVHWDeviceType.AV_HWDEVICE_TYPE_VAAPI => AVPixelFormat.AV_PIX_FMT_VAAPI,
+                AVHWDeviceType.AV_HWDEVICE_TYPE_DXVA2 => AVPixelFormat.AV_PIX_FMT_NV12,
+                AVHWDeviceType.AV_HWDEVICE_TYPE_QSV => AVPixelFormat.AV_PIX_FMT_QSV,
+                AVHWDeviceType.AV_HWDEVICE_TYPE_VIDEOTOOLBOX => AVPixelFormat.AV_PIX_FMT_VIDEOTOOLBOX,
+                AVHWDeviceType.AV_HWDEVICE_TYPE_D3D11VA => AVPixelFormat.AV_PIX_FMT_NV12,
+                AVHWDeviceType.AV_HWDEVICE_TYPE_DRM => AVPixelFormat.AV_PIX_FMT_DRM_PRIME,
+                AVHWDeviceType.AV_HWDEVICE_TYPE_OPENCL => AVPixelFormat.AV_PIX_FMT_OPENCL,
+                AVHWDeviceType.AV_HWDEVICE_TYPE_MEDIACODEC => AVPixelFormat.AV_PIX_FMT_MEDIACODEC,
+                _ => AVPixelFormat.AV_PIX_FMT_NONE
+            };
+        }
+    }
+}
---
+++ SeeShark.Example/VideoFrameConverter.cs
@@ -0,0 +1,83 @@
+using System.Drawing;
+using System.Runtime.InteropServices;
+using FFmpeg.AutoGen;
+
+namespace SeeShark.Example
+{
+    public sealed unsafe class VideoFrameConverter : IDisposable
+    {
+        private readonly IntPtr convertedFrameBufferPtr;
+        private readonly int dstWidth;
+        private readonly int dstHeight;
+        private readonly byte_ptrArray4 dstData;
+        private readonly int_array4 dstLinesize;
+        private readonly SwsContext* pConvertContext;
+
+        public VideoFrameConverter(int srcWidth, int srcHeight, AVPixelFormat srcPixelFormat,
+            int dstWidth, int dstHeight, AVPixelFormat dstPixelFormat)
+        {
+            this.dstWidth = dstWidth;
+            this.dstHeight = dstHeight;
+
+            pConvertContext = ffmpeg.sws_getContext(
+                srcWidth,
+                srcHeight,
+                srcPixelFormat,
+                dstWidth,
+                dstHeight,
+                dstPixelFormat,
+                ffmpeg.SWS_FAST_BILINEAR,
+                null,
+                null,
+                null);
+            if (pConvertContext == null)
+                throw new ApplicationException("Could not initialize the conversion context.");
+
+            var convertedFrameBufferSize = ffmpeg.av_image_get_buffer_size(dstPixelFormat,
+                dstWidth,
+                dstHeight,
+                1);
+            convertedFrameBufferPtr = Marshal.AllocHGlobal(convertedFrameBufferSize);
+            dstData = new byte_ptrArray4();
+            dstLinesize = new int_array4();
+
+            ffmpeg.av_image_fill_arrays(ref dstData,
+                ref dstLinesize,
+                (byte*) convertedFrameBufferPtr,
+                dstPixelFormat,
+                dstWidth,
+                dstHeight,
+                1);
+        }
+
+        public void Dispose()
+        {
+            Marshal.FreeHGlobal(convertedFrameBufferPtr);
+            ffmpeg.sws_freeContext(pConvertContext);
+        }
+
+        public AVFrame Convert(AVFrame sourceFrame)
+        {
+            ffmpeg.sws_scale(pConvertContext,
+                sourceFrame.data,
+                sourceFrame.linesize,
+                0,
+                sourceFrame.height,
+                dstData,
+                dstLinesize);
+
+            var data = new byte_ptrArray8();
+            data.UpdateFrom(dstData);
+            var linesize = new int_array8();
+            linesize.UpdateFrom(dstLinesize);
+
+            return new AVFrame
+            {
+                data = data,
+                linesize = linesize,
+                width = dstWidth,
+                height = dstHeight
+            };
+        }
+    }
+}
