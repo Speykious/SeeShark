5323378035b586f9c7ff42ace10eb1a6baf77f7c
Speykious <speykious@gmail.com>
2021-11-06 02:33:32 +0100

    Create `FFmpegManager` static class
    Makes it a bit easier to setup FFmpeg.

---
+++ SeeShark/FFmpeg/FFmpegManager.cs
@@ -0,0 +1,45 @@
+using System.Runtime.InteropServices;
+using FFmpeg.AutoGen;
+
+namespace SeeShark.FFmpeg
+{
+    public static class FFmpegManager
+    {
+        public static bool IsFFmpegSetup { get; private set; } = false;
+
+        public static string FFmpegVersion => ffmpeg.av_version_info();
+        public static string FFmpegRootPath
+        {
+            get => ffmpeg.RootPath;
+            private set => ffmpeg.RootPath = value;
+        }
+
+        public static void SetupFFmpeg(string rootPath = "/usr/lib", FFmpegLogLevel logLevel = FFmpegLogLevel.Panic, ConsoleColor logColor = ConsoleColor.Yellow)
+        {
+            FFmpegRootPath = rootPath;
+            SetupFFmpegLogging(logLevel, logColor);
+        }
+
+        internal static unsafe void SetupFFmpegLogging(FFmpegLogLevel logLevel, ConsoleColor logColor)
+        {
+            ffmpeg.av_log_set_level((int)logLevel);
+
+            // do not convert to local function
+            av_log_set_callback_callback logCallback = (p0, level, format, vl) =>
+            {
+                if (level > ffmpeg.av_log_get_level()) return;
+
+                var lineSize = 1024;
+                var lineBuffer = stackalloc byte[lineSize];
+                var printPrefix = 1;
+                ffmpeg.av_log_format_line(p0, level, format, vl, lineBuffer, lineSize, &printPrefix);
+                var line = Marshal.PtrToStringAnsi((IntPtr)lineBuffer);
+                Console.ForegroundColor = logColor;
+                Console.Write(line);
+                Console.ResetColor();
+            };
+
+            ffmpeg.av_log_set_callback(logCallback);
+        }
+    }
+}
