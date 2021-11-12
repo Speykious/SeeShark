1a1815b8ee79e6cf010662781aaaf8f315f78e3d
Speykious <speykious@gmail.com>
2021-11-12 17:53:59 +0100

    Make AVFrame internal

--- SeeShark/Frame.cs
+++ SeeShark/Frame.cs
@@ -8,31 +8,36 @@ namespace SeeShark
 {
     public sealed unsafe class Frame : IDisposable
     {
-        private readonly AVFrame* avFrame;
+        internal readonly AVFrame* AVFrame;
 
-        public int Width => avFrame->width;
-        public int Height => avFrame->height;
-        public int WidthStep => avFrame->linesize[0];
-        public PixelFormat PixelFormat => (PixelFormat)avFrame->format;
-        public ReadOnlySpan<byte> RawData => new ReadOnlySpan<byte>(avFrame->data[0], WidthStep * Height);
+        public int Width => AVFrame->width;
+        public int Height => AVFrame->height;
+        public int WidthStep => AVFrame->linesize[0];
+        public PixelFormat PixelFormat => (PixelFormat)AVFrame->format;
+        public ReadOnlySpan<byte> RawData => new ReadOnlySpan<byte>(AVFrame->data[0], WidthStep * Height);
 
         // This constructor is internal because the user of the library
         // is not supposed to deal with any actual FFmpeg type.
         internal Frame()
         {
-            avFrame = ffmpeg.av_frame_alloc();
+            AVFrame = ffmpeg.av_frame_alloc();
+        }
+
+        internal Frame(AVFrame* avFrame)
+        {
+            AVFrame = avFrame;
         }
 
         public void Dispose()
         {
-            var frame = avFrame;
+            var frame = AVFrame;
             ffmpeg.av_frame_free(&frame);
         }
 
-        internal int HardwareAccelCopyTo(Frame hwFrame) => ffmpeg.av_hwframe_transfer_data(hwFrame.avFrame, avFrame, 0);
+        internal int HardwareAccelCopyTo(Frame hwFrame) => ffmpeg.av_hwframe_transfer_data(hwFrame.AVFrame, AVFrame, 0);
 
-        internal void Unref() => ffmpeg.av_frame_unref(avFrame);
+        internal void Unref() => ffmpeg.av_frame_unref(AVFrame);
 
-        internal int Receive(AVCodecContext* codecContext) => ffmpeg.avcodec_receive_frame(codecContext, avFrame);
+        internal int Receive(AVCodecContext* codecContext) => ffmpeg.avcodec_receive_frame(codecContext, AVFrame);
     }
 }
