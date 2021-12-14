c89089915b01f2c5ec483a0962d8a6386f20cb99
Adwyzz (OLED Edition) <46694241+adryzz@users.noreply.github.com>
2021-12-14 23:02:23 +0100

    hide cursor for a better viewing experience

--- SeeShark.Example.Ascii/Program.cs
+++ SeeShark.Example.Ascii/Program.cs
@@ -101,6 +101,7 @@ namespace SeeShark.Example.Ascii
                         loop = false;
                         break;
                 }
+                Console.CursorVisible = karen.IsPlaying;
             }
 
             Dispose();
