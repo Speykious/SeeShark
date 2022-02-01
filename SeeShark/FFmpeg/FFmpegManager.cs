749fee8d4d3726e24b5a2074f8385d68949b91b2
Speykious <speykious@gmail.com>
2022-02-01 12:46:18 +0100

    Use XML doc

--- SeeShark/FFmpeg/FFmpegManager.cs
+++ SeeShark/FFmpeg/FFmpegManager.cs
@@ -118,10 +118,11 @@ namespace SeeShark.FFmpeg
             return libraries.ToStrings().All((lib) => canLoadLibrary(lib, path, validated));
         }
 
-        // Note: dependencies are not checked as they are optional in FFmpeg.AutoGen.
-        // See following links:
-        // - https://github.com/Ruslan-B/FFmpeg.AutoGen/commit/395dea80c642c85e089e3d7721f91d77594655c1
-        // - https://github.com/Ruslan-B/FFmpeg.AutoGen/blob/633c15d323785092561329ad4b5742b0189116d6/FFmpeg.AutoGen/FFmpeg.cs#L57-L82
+        /// <remarks>
+        /// Note: dependencies are not checked as they are optional in FFmpeg.AutoGen.
+        /// See <see href="https://github.com/Ruslan-B/FFmpeg.AutoGen/commit/395dea80c642c85e089e3d7721f91d77594655c1">the following commit</see>
+        /// and <see href="https://github.com/Ruslan-B/FFmpeg.AutoGen/blob/633c15d323785092561329ad4b5742b0189116d6/FFmpeg.AutoGen/FFmpeg.cs#L57-L82">this function</see>
+        /// </remarks>
         private static bool canLoadLibrary(string lib, string path, List<string> validated)
         {
             if (validated.Contains(lib))
