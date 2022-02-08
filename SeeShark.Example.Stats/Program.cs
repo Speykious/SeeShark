e867e08e60f8a1f6d44ecd5a1111067779309115
Speykious <speykious@gmail.com>
2022-02-08 13:56:24 +0100

    dotnet format... ok

--- SeeShark.Example.Stats/Program.cs
+++ SeeShark.Example.Stats/Program.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) The Vignette Authors
+// Copyright (c) The Vignette Authors
 // This file is part of SeeShark.
 // SeeShark is licensed under the BSD 3-Clause License. See LICENSE for details.
 
