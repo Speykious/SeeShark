4b9ad1fe5e4ca2ca46c9d274d99697813422ce9f
Speykious <speykious@gmail.com>
2022-02-18 13:51:31 +0100

    Add XML documentation

--- SeeShark/VideoInputOptions.cs
+++ SeeShark/VideoInputOptions.cs
@@ -7,12 +7,41 @@ using FFmpeg.AutoGen;
 
 namespace SeeShark
 {
+    /// <summary>
+    /// Options to give to a <see cref="VideoStreamDecoder" /> for it to feed them to FFmpeg when opening a video input stream.
+    /// We can indeed open an input in different ways. For example, you might want to open your camera at a different resolution, or change the input format.
+    /// </summary>
+    /// <remarks>
+    /// Some examples of input options are:
+    /// https://ffmpeg.org/ffmpeg-devices.html#video4linux2_002c-v4l2
+    /// https://ffmpeg.org/ffmpeg-devices.html#dshow
+    /// https://ffmpeg.org/ffmpeg-devices.html#avfoundation
+    /// </remarks>
     public class VideoInputOptions
     {
+        /// <summary>
+        /// To request a specific resolution of the video stream.
+        /// </summary>
+        /// <remarks>
+        /// The underlying driver will change it back to a compatible resolution.
+        /// </remarks>
+        /// <value>(width, height)</value>
         public (int, int)? VideoSize { get; set; }
+        /// <summary>
+        /// To request a specific framerate for the video stream.
+        /// </summary>
+        /// <remarks>
+        /// The underlying driver will change it back to a compatible framerate.
+        /// </remarks>
         public AVRational? Framerate { get; set; }
+        /// <summary>
+        /// To request a specific input format for the video stream.
+        /// </summary>
         public string? InputFormat { get; set; }
 
+        /// <summary>
+        /// Combines all properties into a dictionary of options that FFmpeg can use.
+        /// </summary>
         public virtual IDictionary<string, string> ToAVDictOptions()
         {
             Dictionary<string, string> dict = new();
